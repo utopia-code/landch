98999ee5830bd136f1a1028a43c7d168ff13dbfd
utopia-code <utopia-code.developer@gmail.com>
2025-06-11 22:04:17 +0200

    test online loading home videos

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { AfterViewInit, Component, ElementRef, OnInit, QueryList, ViewChildren } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, ElementRef, NgZone, OnInit, QueryList, ViewChildren } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ListComponent } from '../galleries/list/list.component';
 
@@ -15,61 +15,123 @@ export class HomeComponent implements OnInit,AfterViewInit{
 
   listVisible = false;
   isMobile = false;
+  loading = true;
+
+  constructor(private cdr: ChangeDetectorRef, private ngZone: NgZone) {}
+
 
   ngOnInit(): void {
     this.isMobile = window.innerWidth <= 768;
     
-    setTimeout(() => {
-      this.listVisible = true;
-    }, 3000);
+    // setTimeout(() => {
+    //   this.listVisible = true;
+    // }, 3000);
   }
 
   ngAfterViewInit() {
-    if ('IntersectionObserver' in window) {
-      const observer = new IntersectionObserver((entries) => {
-        entries.forEach(entry => {
-          const video = entry.target as HTMLVideoElement;
+    const videoElements = this.lazyVideos.map(ref => ref.nativeElement);
 
-          if (entry.isIntersecting) {
-            if (video.readyState >= 3) {
-              video.play().catch(err => {
-                console.warn('Unable to play automatically:', err);
-              });
-            } else {
-              const onCanPlay = () => {
-                video.play().catch(err => {
-                  console.warn('Unable to play automatically:', err);
-                });
-                video.removeEventListener('canplaythrough', onCanPlay);
-              };
-              video.addEventListener('canplaythrough', onCanPlay);
-              video.load();
-            }
-          } else {
-            video.pause();
-          }
-        });
-      }, {
-        threshold: 0.1,
+    const loadPromises = videoElements.map(video => {
+      return new Promise<void>((resolve) => {
+        const onReady = () => {
+          video.removeEventListener('canplaythrough', onReady);
+          resolve();
+        };
+        if (video.readyState >= 3) {
+          resolve();
+        } else {
+          video.addEventListener('canplaythrough', onReady);
+          video.load();
+        }
       });
+    });
 
-      if ('requestIdleCallback' in window) {
-        requestIdleCallback(() => {
-          this.lazyVideos.forEach(videoRef => {
-            observer.observe(videoRef.nativeElement);
-          });
-        });
-      } else {
-        
+    Promise.all(loadPromises).then(() => {
+      this.ngZone.run(() => {
+        this.loading = false;
         setTimeout(() => {
-          this.lazyVideos.forEach(videoRef => {
-            observer.observe(videoRef.nativeElement);
-          });
-        }, 200);
-      }
+          this.listVisible = true;
+        }, 3000);
+      });
+    });
+  }
+
+  // ngAfterViewInit() {
+  //   if ('IntersectionObserver' in window) {
+  //     const observer = new IntersectionObserver((entries) => {
+  //       entries.forEach(entry => {
+  //         const video = entry.target as HTMLVideoElement;
+
+  //         if (entry.isIntersecting) {
+  //           if (video.readyState >= 3) {
+  //             video.play().catch(err => {
+  //               console.warn('Unable to play automatically:', err);
+  //             });
+  //           } else {
+  //             const onCanPlay = () => {
+  //               video.play().catch(err => {
+  //                 console.warn('Unable to play automatically:', err);
+  //               });
+  //               video.removeEventListener('canplaythrough', onCanPlay);
+  //             };
+  //             video.addEventListener('canplaythrough', onCanPlay);
+  //             video.load();
+  //           }
+  //         } else {
+  //           video.pause();
+  //         }
+  //       });
+  //     }, {
+  //       threshold: 0.1,
+  //     });
+
+  //     const waitForVideos = () => {
+  //       const videoElements = this.lazyVideos.map(v => v.nativeElement);
+  //       const promises = videoElements.map(video => {
+  //         return new Promise<void>((resolve) => {
+  //           if (video.readyState >= 3) {
+  //             resolve();
+  //           } else {
+  //             const onReady = () => {
+  //               resolve();
+  //               video.removeEventListener('canplaythrough', onReady);
+  //             };
+  //             video.addEventListener('canplaythrough', onReady);
+  //             video.load();
+  //           }
+  //         });
+  //       });
+
+  //       // Promise.all(promises).then(() => {
+  //       //   this.loading = false;
+  //       //   this.lazyVideos.forEach(videoRef => observer.observe(videoRef.nativeElement));
+  //       // });
+
+  //       Promise.race([
+  //         Promise.all(promises),
+  //         new Promise<void>(resolve => setTimeout(resolve, 6000))
+  //       ]).then(() => {
+  //         this.loading = false;
+        
+  //         setTimeout(() => {
+  //           this.ngZone.run(() => {
+  //             this.listVisible = true;
+  //             this.cdr.detectChanges();
+  //           });
+  //         }, 3000);
+        
+  //         this.lazyVideos.forEach(videoRef => observer.observe(videoRef.nativeElement));
+  //       });
+  //     };
+
+  //     if ('requestIdleCallback' in window) {
+  //       requestIdleCallback(waitForVideos);
+  //     } else {
+  //       setTimeout(waitForVideos, 200);
+  //     }
 
     
-    }
-  }
+  //   }
+  // }
 
 }
